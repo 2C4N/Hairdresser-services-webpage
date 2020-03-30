5c8c63565f0804df141813b993ddb11ed573c04b
2C4N <37574083+2C4N@users.noreply.github.com>
2020-03-30 21:47:03 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+var menuElements = ["Strona Główna", "Oferta", "Galeria", "Opinie", "Kontakt"];
+var menuLinks = [
+  "index.html",
+  "oferta.html",
+  "galeria.html",
+  "opinie.html",
+  "kontakt.html"
+];
+var priceName = [
+  "Strzyżenie damskie",
+  "Strzyżenie męskie",
+  "Modelowanie",
+  "Koloryzacja",
+  "Trwała",
+  "Prostowanie keratynowe",
+  "Mycie głowy"
+];
+var pricePrice = [
+  "65zł | 70zł | 75zł",
+  "20zł | 25zł | 30zł",
+  "50zł | 55zł | 65zł",
+  "170zł | 180zł | 220zł",
+  "150zł | 170zł | 190zł",
+  "360zł | 460zł | 560zł",
+  "5zł | 10zł | 15zł"
+];
+
+function changePage(page) {
+  window.location.href = menuLinks[page];
+}
+
+function menuGenerator() {
+  var row = document.createElement("tr");
+  for (i = 0; i < menuElements.length; i++) {
+    var element = document.createElement("td");
+    var text = document.createTextNode(menuElements[i]);
+    element.id = i;
+    element.onclick = function() {
+      changePage(this.id);
+    };
+
+    element.appendChild(text);
+    row.appendChild(element);
+    document.getElementById("menu").appendChild(row);
+  }
+}
+
+function stars(focusID, stars) {
+  var div = document.createElement("div");
+  div.className = "stars";
+  for (i = 1; i <= 5; i++) {
+    var star = document.createElement("ion-icon");
+    star.name = "star";
+    star.id = i;
+    star.style.marginRight = "4px";
+
+    if (star.id > stars) {
+      star.style.color = getComputedStyle(
+        document.documentElement
+      ).getPropertyValue("--secondary-color");
+    }
+
+    div.appendChild(star);
+    document.getElementById(focusID).appendChild(div);
+  }
+}
+
+function priceGenerator() {
+  for (i = 0; i < priceName.length; i++) {
+    var row = document.createElement("tr");
+    var name = document.createElement("td");
+    var price = document.createElement("td");
+    var nameText = document.createTextNode(priceName[i]);
+    var priceText = document.createTextNode(pricePrice[i]);
+    name.appendChild(nameText);
+    price.appendChild(priceText);
+    row.appendChild(name);
+    row.appendChild(price);
+    name.style.width = "65%";
+    name.className = "priceText";
+    price.style.width = "35%";
+    price.style.textAlign = "center";
+    row.style.width = "100%";
+    row.style.height = "35px";
+    document.getElementById("prices").appendChild(row);
+  }
+}
+
+//automatyzacja galerii?
